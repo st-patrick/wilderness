0a3b391ed51253035dcace218820105dbc535186
Patrick <st-patrick@users.noreply.github.com>
2023-08-24 16:34:36 -0700

    collision works .... more or less

--- wilderness.py
+++ wilderness.py
@@ -6,14 +6,12 @@ import math
 pygame.init()
 
 # Constants
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_SIZE = 600  # Screen will be square
 PLAYER_SIZE = 40
-ENEMY_SIZE = 30
-BLOCK_SIZE = 50
+ENEMY_SIZE = 40  # Increase enemy size for easier hit detection
 FPS = 30
 ATTACK_DISTANCE = 50
-ENEMY_MAX_HEALTH = 3
+ENEMY_MAX_HEALTH = 5  # Increase enemy health
 
 # Colors
 WHITE = (255, 255, 255)
@@ -22,13 +20,13 @@ GREEN = (0, 255, 0)
 BROWN = (139, 69, 19)
 BLACK = (0, 0, 0)
 
-# Create the screen
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Create the screen (square)
+screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
 pygame.display.set_caption("Adventures in the Wilderness")
 
 # Player starting position
-player_x = SCREEN_WIDTH // 2
-player_y = SCREEN_HEIGHT // 2
+player_x = SCREEN_SIZE // 2
+player_y = SCREEN_SIZE // 2
 
 # Background map (0: path, 1: tree)
 background_map = [
@@ -44,6 +42,12 @@ enemy_x = 200
 enemy_y = 300
 enemy_health = ENEMY_MAX_HEALTH
 
+# Calculate block size based on screen size and map length
+block_size = SCREEN_SIZE // len(background_map)
+
+# Calculate player collision size
+player_collision_size = PLAYER_SIZE // 2
+
 # Main game loop
 clock = pygame.time.Clock()
 running = True
@@ -55,26 +59,46 @@ while running:
 
     # Player movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_x > 0:
-        player_x -= 5
-    if keys[pygame.K_RIGHT] and player_x < SCREEN_WIDTH - PLAYER_SIZE:
-        player_x += 5
-    if keys[pygame.K_UP] and player_y > 0:
-        player_y -= 5
-    if keys[pygame.K_DOWN] and player_y < SCREEN_HEIGHT - PLAYER_SIZE:
-        player_y += 5
+    new_player_x = player_x
+    new_player_y = player_y
+    if keys[pygame.K_LEFT] and player_x - player_collision_size > 0:
+        new_player_x -= 5
+    if keys[pygame.K_RIGHT] and player_x + player_collision_size < SCREEN_SIZE:
+        new_player_x += 5
+    if keys[pygame.K_UP] and player_y - player_collision_size > 0:
+        new_player_y -= 5
+    if keys[pygame.K_DOWN] and player_y + player_collision_size < SCREEN_SIZE:
+        new_player_y += 5
+
+    # Check if new player position is valid
+    player_top_left = (new_player_x - player_collision_size, new_player_y - player_collision_size)
+    player_top_right = (new_player_x + player_collision_size, new_player_y - player_collision_size)
+    player_bottom_left = (new_player_x - player_collision_size, new_player_y + player_collision_size)
+    player_bottom_right = (new_player_x + player_collision_size, new_player_y + player_collision_size)
+
+    invalid_move = False
+    for point in [player_top_left, player_top_right, player_bottom_left, player_bottom_right]:
+        row = point[1] // block_size
+        col = point[0] // block_size
+        if background_map[row][col] == 1:
+            invalid_move = True
+            break
+
+    if not invalid_move:
+        player_x = new_player_x
+        player_y = new_player_y
 
     # Draw background
     screen.fill(WHITE)
     for row in range(len(background_map)):
         for col in range(len(background_map[0])):
             if background_map[row][col] == 0:
-                pygame.draw.rect(screen, WHITE, (col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+                pygame.draw.rect(screen, WHITE, (col * block_size, row * block_size, block_size, block_size))
             elif background_map[row][col] == 1:
-                pygame.draw.rect(screen, BROWN, (col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+                pygame.draw.rect(screen, BROWN, (col * block_size, row * block_size, block_size, block_size))
 
     # Draw the player
-    pygame.draw.rect(screen, GREEN, (player_x, player_y, PLAYER_SIZE, PLAYER_SIZE))
+    pygame.draw.rect(screen, GREEN, (player_x - player_collision_size, player_y - player_collision_size, PLAYER_SIZE, PLAYER_SIZE))
 
     # Calculate distance to enemy
     distance_to_enemy = math.sqrt((player_x - enemy_x)**2 + (player_y - enemy_y)**2)
