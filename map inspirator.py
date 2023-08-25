64e4e1a2e4508e3082deea9be19502c9c056ec45
Patrick <st-patrick@users.noreply.github.com>
2023-08-24 17:02:33 -0700

    MVP 2

--- map inspirator.py
+++ map inspirator.py
@@ -1,67 +1,78 @@
 import pygame
 import random
 
+# Initialize pygame
 pygame.init()
 
-# Constants
-SCREEN_SIZE = 800
-MAP_SIZE = 10
-BLOCK_SIZE = SCREEN_SIZE // MAP_SIZE
-BLOCK_PROBABILITY = 0.3
+# Define constants
+WIDTH, HEIGHT = 800, 600
+BLOCK_SIZE = 20
+MAP_WIDTH, MAP_HEIGHT = WIDTH // BLOCK_SIZE, HEIGHT // BLOCK_SIZE
+PLAYER_SPEED = 10  # Adjust player speed (higher value = slower movement)
+MOVE_DELAY = 10   # Frames to wait before moving player
 
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# Create a new map
+def generate_map(density):
+    return [[1 if random.random() < density else 0 for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
 
-# Generate initial map
-def generate_map(size):
-    return [[1 if random.random() < BLOCK_PROBABILITY else 0 for _ in range(size)] for _ in range(size)]
+# Initialize player position
+player_x, player_y = MAP_WIDTH // 2, MAP_HEIGHT // 2
+move_counter = 0
 
-# Draw the map
-def draw_map(screen, game_map):
-    for y in range(MAP_SIZE):
-        for x in range(MAP_SIZE):
-            color = WHITE if game_map[y][x] == 0 else BLACK
-            pygame.draw.rect(screen, color, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+# Initialize screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Random Map Generator")
 
-# Main function
-def main():
-    screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
-    pygame.display.set_caption("Random Map Generator")
+# Main loop
+running = True
+density = 0.4  # Initial map density
+current_map = generate_map(density)
 
-    clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-    current_map = generate_map(MAP_SIZE)
-    player_x, player_y = MAP_SIZE // 2, MAP_SIZE // 2
+    keys = pygame.key.get_pressed()
+    dx, dy = 0, 0
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    if keys[pygame.K_UP]:
+        dy -= 1
+    if keys[pygame.K_DOWN]:
+        dy += 1
+    if keys[pygame.K_LEFT]:
+        dx -= 1
+    if keys[pygame.K_RIGHT]:
+        dx += 1
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_y -= 1
-        if keys[pygame.K_s]:
-            player_y += 1
-        if keys[pygame.K_a]:
-            player_x -= 1
-        if keys[pygame.K_d]:
-            player_x += 1
+    move_counter += 1
+    if move_counter >= MOVE_DELAY:
+        # Check if the new position is valid (not a black block)
+        new_x = player_x + dx
+        new_y = player_y + dy
 
-        if player_x < 0 or player_x >= MAP_SIZE or player_y < 0 or player_y >= MAP_SIZE:
-            current_map = generate_map(MAP_SIZE)
-            player_x, player_y = MAP_SIZE // 2, MAP_SIZE // 2
+        if 0 <= new_x < MAP_WIDTH and 0 <= new_y < MAP_HEIGHT and current_map[new_y][new_x] == 0:
+            player_x = new_x
+            player_y = new_y
 
-        screen.fill(WHITE)
-        draw_map(screen, current_map)
-        pygame.draw.rect(screen, (255, 0, 0), (player_x * BLOCK_SIZE, player_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-        pygame.display.flip()
+        move_counter = 0
 
-        clock.tick(10)
+    # Check if player is walking off screen
+    if player_x < 0 or player_x >= MAP_WIDTH or player_y < 0 or player_y >= MAP_HEIGHT:
+        density -= 0.05  # Adjust density for next map
+        current_map = generate_map(density)
+        player_x, player_y = MAP_WIDTH // 2, MAP_HEIGHT // 2
 
-    pygame.quit()
+    # Draw the map
+    screen.fill((255, 255, 255))  # Set background to white
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            if current_map[y][x] == 1:
+                pygame.draw.rect(screen, (0, 0, 0), (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+    pygame.draw.rect(screen, (255, 0, 0), (player_x * BLOCK_SIZE, player_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
 
-if __name__ == "__main__":
-    main()
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)  # Limit the frame rate
+
+# Clean up
+pygame.quit()
